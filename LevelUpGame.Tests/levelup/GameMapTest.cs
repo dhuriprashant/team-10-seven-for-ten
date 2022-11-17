7a900e872f4508ecec1f37d307985bd9b555ec8b
Prashant Dhuri <dhuriprashant@gmail.com>
2022-11-17 21:25:11 +0000

    added game map and test

---
+++ LevelUpGame.Tests/levelup/GameMapTest.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using levelup;
+
+namespace levelup
+{
+    [TestFixture]
+    public class GameMapTest
+    {
+        private GameMap? testObj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new GameMap();
+        }
+
+        [Test]
+        public void IsGameMapInitialized()
+        {
+#pragma warning disable CS8602 // Rethrow to preserve stack details
+            
+            testObj.Init ();
+
+            Assert.AreEqual(100,testObj.numPositions);
+            
+        }
+    }
+}
---
+++ LevelUpGame/levelup/GameMap.cs
@@ -0,0 +1,9 @@
+namespace levelup
+{
+    public class GameMap{
+        public int numPositions;
+        public void Init(){
+            numPositions = 100;
+        }    
+    }
+}
