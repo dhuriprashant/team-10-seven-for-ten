2efd84f35fb077379fcb7420b70a9676c5b605b1
Estelle Council <you@example.com>
2022-11-18 00:15:10 +0000

    Game Status

---
+++ LevelUpGame.Tests/levelup/GameStatusTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using levelup;
+using System.Drawing;
+
+namespace levelup
+{
+    [TestFixture]
+    public class GameStatusTest{
+        private Position? testObj;
+        String Name;
+        int numberOfMoves;
+            
+        [Test]
+        public void IsNameHaveValue(){
+            testName = new Name();
+
+            Assert.NotNull(testName.Name);
+        }
+
+        [Test]
+        public void NumberOfMoves() {
+            testMoves = new NumberOfMoves();
+
+            Assert.NotNull(testMoves.NumberOfMoves);
+        }
+
+        [
+            
+        ]
+        
